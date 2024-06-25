31117c647aa32403c868ea8457d0e94037d1a10d
BobbyCAI <381504435@qq.com>
2024-06-25 18:19:24 +0800

    delete chat-gpt repository

--- src/model/modules/01-llm.ts
+++ src/model/modules/01-llm.ts
@@ -1,7 +1,6 @@
 export default <AppItem[]>[
   {
     homepage: 'https://chatgpt.com',
-    repository: 'https://chatgpt.com',
     icon: 'chat-gpt.png',
     keywords: ['chatgpt'],
     name: 'ChatGPT',
