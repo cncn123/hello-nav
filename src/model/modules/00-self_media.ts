623ca138517dc64ab88b1752c2f6892f2886cf6d
BobbyCAI <381504435@qq.com>
2024-06-28 17:59:49 +0800

    add xiaohongshu-download.png

---
+++ src/model/modules/00-self_media.ts
@@ -0,0 +1,9 @@
+export default <AppItem[]>[
+  {
+    homepage: 'https://www.xhs-download.online',
+    icon: 'xiaohongshu-download.png',
+    keywords: ['xhs','小红书','xiaohongshu'],
+    name: '小红书去水印图片视频',
+  },
+
+]
